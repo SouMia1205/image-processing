a82d1d602364386ab246362c665f739bec8703f0
SouMia1205 <laksoumia31@gmail.com>
2025-03-07 17:39:13 +0100

    Update Image Processing Project -- 2

--- main.py
+++ main.py
@@ -7,38 +7,88 @@ def lire_image(chemin_image):
     return image.convert("RGB")  # S'assurer qu'elle est bien en RGB
 
 def afficher_image_et_histogramme(image, titre):
-    """ Affiche une image et son histogramme. """
-    # Affichage de l'image
+    """ Affiche une image et son histogramme (RGB ou niveaux de gris). """
     plt.figure(figsize=(12, 5))
 
-    plt.subplot(1, 2, 1)  # Première partie : Image
-    plt.imshow(image, cmap="gray" if image.mode == "L" else None)  # Grayscale si mode "L"
+    # Affichage de l'image
+    plt.subplot(1, 2, 1)
+    plt.imshow(image, cmap="gray" if image.mode == "L" else None)
     plt.axis("off")
     plt.title(titre)
 
-    # Calcul de l'histogramme
-    histogramme = [0] * 256
     largeur, hauteur = image.size
 
-    for x in range(largeur):
-        for y in range(hauteur):
-            if image.mode == "L":
-                gris = image.getpixel((x, y))
-            else:
+    if image.mode == "RGB":
+        # Histogramme en RGB
+        hist_r, hist_g, hist_b = [0] * 256, [0] * 256, [0] * 256
+
+        for x in range(largeur):
+            for y in range(hauteur):
                 r, g, b = image.getpixel((x, y))
-                gris = int(0.299 * r + 0.587 * g + 0.114 * b)
+                hist_r[r] += 1
+                hist_g[g] += 1
+                hist_b[b] += 1
 
-            histogramme[gris] += 1
+        # Affichage des trois histogrammes
+        plt.subplot(1, 2, 2)
+        plt.plot(range(256), hist_r, color="red", label="Rouge")
+        plt.plot(range(256), hist_g, color="green", label="Vert")
+        plt.plot(range(256), hist_b, color="blue", label="Bleu")
+        plt.xlabel("Valeur du pixel")
+        plt.ylabel("Nombre de pixels")
+        plt.title("Histogramme RGB")
+        plt.legend()
 
-    # Affichage de l'histogramme
-    plt.subplot(1, 2, 2)
-    plt.bar(range(256), histogramme, color='red')
-    plt.xlabel("Niveaux de gris")
-    plt.ylabel("Nombre de pixels")
-    plt.title("Histogramme de " + titre)
+    else:
+        # Histogramme en niveaux de gris
+        histogramme = [0] * 256
+        for x in range(largeur):
+            for y in range(hauteur):
+                gris = image.getpixel((x, y))
+                histogramme[gris] += 1
 
-    # Affichage des deux parties
+        plt.subplot(1, 2, 2)
+        plt.bar(range(256), histogramme, color="gray")
+        plt.xlabel("Niveaux de gris")
+        plt.ylabel("Nombre de pixels")
+        plt.title("Histogramme Niveaux de Gris")
+
     plt.show()
+
+# def afficher_image_et_histogramme(image, titre):
+#     """ Affiche une image et son histogramme. """
+#     # Affichage de l'image
+#     plt.figure(figsize=(12, 5))
+
+#     plt.subplot(1, 2, 1)  # Première partie : Image
+#     plt.imshow(image, cmap="gray" if image.mode == "L" else None)  # Grayscale si mode "L"
+#     plt.axis("off")
+#     plt.title(titre)
+
+#     # Calcul de l'histogramme
+#     histogramme = [0] * 256
+#     largeur, hauteur = image.size
+
+#     for x in range(largeur):
+#         for y in range(hauteur):
+#             if image.mode == "L":
+#                 gris = image.getpixel((x, y))
+#             else:
+#                 r, g, b = image.getpixel((x, y))
+#                 gris = int(0.299 * r + 0.587 * g + 0.114 * b)
+
+#             histogramme[gris] += 1
+
+#     # Affichage de l'histogramme
+#     plt.subplot(1, 2, 2)
+#     plt.bar(range(256), histogramme, color='red')
+#     plt.xlabel("Niveaux de gris")
+#     plt.ylabel("Nombre de pixels")
+#     plt.title("Histogramme de " + titre)
+
+#     # Affichage des deux parties
+#     plt.show()
+
 
 def convertir_en_niveaux_de_gris(image):
     """ Convertit une image en niveaux de gris en utilisant getpixel() et putpixel(). """
